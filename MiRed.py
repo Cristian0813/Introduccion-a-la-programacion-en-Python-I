a1cca5b8ce2fd61a9d92fac1037c0c68383ecef9
Cristian Arias <javier.0813@hotmail.com>
2024-05-15 22:42:40 -0500

    Mi Red Toy Friends

--- MiRed.py
+++ MiRed.py
@@ -1,46 +1,32 @@
-# Mensaje de bienvenida
-print("Bienvenido a ... ")
-print("""
-              _                  __   _________                _____                  __     
-   ____ ___  (_)  ________  ____/ /  /___  ___/_________  ____/ ___/_____(_)___  ____/ /_____
-  / __ `__ \/ /  / ___/ _ \/ __  /      / /   / __  /___`´___/ __/ / ___/ / __ \/ __  /_____/
- / / / / / / /  / /  /  __/ /_/ /      / /   / /_/ /   / /  / /   / /  / /  ___/ /_/ /____ /
-/_/ /_/ /_/_/  /_/   \___/\__,_/      /_/   /_____/   /_/  /_/   /_/  /_/\____/\__,_/_____/  
-              
-""")
+import RedToyFriends as Red
 
-# Función para calcular la edad
-def calcular_edad(agno_nacimiento):
-    return 2024 - agno_nacimiento
+#Aquí­ empieza el programa principal.
+Red.mostrar_bienvenida()
+(nombre, edad, estatura_m, estatura_cm, num_amigos, genero, num_telefono, ciudad, pais) = Red.obtener_datos()
 
-# Función para convertir la estatura a metros y centímetros
-def convertir_estatura(estatura):
-    metros = int(estatura)
-    centimetros = int((estatura - metros) * 100)
-    return metros, centimetros
+print("Muy bien,", nombre, ". Entonces podemos crear un perfil con estos datos.")
+mensajes_amigos = {}
+mensaje_publico = ""
+Red.mostrar_perfil(nombre, edad, estatura_m, estatura_cm, num_amigos, genero, num_telefono, ciudad, pais, mensaje_publico, mensajes_amigos)
+print("Ya podemos preguntar, recordar y calcular datos. Esperamos que disfrutes con Mi Red ToyFriends")
+print("--------------------------------------------------")
 
-# Función para actualizar el perfil de usuario
-def actualizar_perfil():
-    nombre = input("Para empezar, dime como te llamas. ")
-    print(f"Hola {nombre}, bienvenido a Mi Red ToyFriends")
-    print()
+opcion = 1
+while opcion != 0:
+    opcion = Red.opcion_menu()
+    if opcion == 1:
+        mensaje_publico = Red.obtener_mensaje()
+    elif opcion == 2:
+        for i in range(num_amigos):
+            nombre_amigo = input("Ingresa el nombre de tu amigo o amiga: ")
+            mensaje_amigo = Red.obtener_mensaje_amigo(nombre_amigo)
+            mensajes_amigos[nombre_amigo] = mensaje_amigo
+    elif opcion == 3:
+        Red.mostrar_perfil(nombre, edad, estatura_m, estatura_cm, num_amigos, genero, num_telefono, ciudad, pais, mensaje_publico, mensajes_amigos)
+    elif opcion == 4:
+        nombre, edad, (estatura_m, estatura_cm), num_amigos, genero, num_telefono, ciudad, pais = Red.obtener_datos()
+        Red.mostrar_perfil(nombre, edad, estatura_m, estatura_cm, num_amigos, genero, num_telefono, ciudad, pais, mensaje_publico, mensajes_amigos)
+    elif opcion == 0:
+        print("Has decidido salir.")
 
-    agno = int(input("Para preparar tu perfil, dime en qué año naciste. "))
-    edad = calcular_edad(agno)
-    print()
-
-    estatura = float(input("Cuéntame más de ti, para agregarlo a tu perfil. ¿Cuánto mides? Dimelo en metros. "))
-    estatura_m, estatura_cm = convertir_estatura(estatura)
-    print()
-    
-    num_amigos = int(input("Muy bien. Finalmente, Cuéntame cuántos amigos tienes. "))
-    print()
-
-    sexo = input("¿Cuál es tu sexo? ")
-    print()
-    
-    num_telefono = input("¿Cuál es tu número de teléfono? ")
-    print()
-    
-    ciudad = input("¿En qué ciudad vives? ")
-    print()
+print("Gracias por usar Mi Red ToyFriends. ¡Hasta pronto!")
